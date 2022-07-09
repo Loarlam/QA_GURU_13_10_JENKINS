6fdbf10b73a35beffca1ef406f1b583c310ef0f4
Loarlam <Loarlam@ya.ru>
2022-07-10 01:11:09 +0300

    Вынесены создания скриншотов и видео в отдельный класс. Добавлен DesiredCapabilities.

--- src/test/java/guru/qa/tests/TestBaseIncludingBeforeAndAfterTests.java
+++ src/test/java/guru/qa/tests/TestBaseIncludingBeforeAndAfterTests.java
@@ -3,8 +3,11 @@ package guru.qa.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.pages.PageOfRegistrationForm;
+import guru.qa.utils.FinalActivitiesForReport;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBaseIncludingBeforeAndAfterTests {
 
@@ -13,9 +16,19 @@ public class TestBaseIncludingBeforeAndAfterTests {
 
     @BeforeAll
     static void beforeAllTests() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
         SelenideLogger.addListener("Allure", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.browserPosition = "0x0";
+    }
+
+    @AfterAll
+    static void afterAllTests() {
+        FinalActivitiesForReport.attachScreenshot();
+        FinalActivitiesForReport.addVideo();
     }
 }
