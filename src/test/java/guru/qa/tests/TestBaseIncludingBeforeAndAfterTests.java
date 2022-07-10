82158ec28e3fbf5561f44c08046994ce9cbf1d70
Loarlam <Loarlam@ya.ru>
2022-07-10 09:48:14 +0300

    Добавлен вывод исходников и консольных логов. Поправлен метод формирования видео.

--- src/test/java/guru/qa/tests/TestBaseIncludingBeforeAndAfterTests.java
+++ src/test/java/guru/qa/tests/TestBaseIncludingBeforeAndAfterTests.java
@@ -3,7 +3,7 @@ package guru.qa.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.pages.PageOfRegistrationForm;
-import guru.qa.utils.FinalActivitiesForReport;
+import guru.qa.attachments.AttachmentsForReport;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -16,7 +16,7 @@ public class TestBaseIncludingBeforeAndAfterTests {
     @BeforeAll
     static void beforeAllTests() {
         SelenideLogger.addListener("Allure", new AllureSelenide());
-        
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.browserPosition = "0x0";
@@ -30,7 +30,9 @@ public class TestBaseIncludingBeforeAndAfterTests {
 
     @AfterAll
     static void afterAllTests() {
-        FinalActivitiesForReport.attachScreenshot();
-        FinalActivitiesForReport.addVideo();
+        AttachmentsForReport.attachScreenshot();
+        AttachmentsForReport.pageSource();
+        AttachmentsForReport.browserConsoleLogs();
+        AttachmentsForReport.addVideo();
     }
 }
